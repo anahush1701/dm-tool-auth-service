df17b92593329270132e46a6fe35a277175e6b5d
Anastasiya Puzikava <anastasiya.hush@gmail.com>
2025-08-21 14:06:54 +0200

    Initial files commit

---
+++ Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthService.Models
+{
+    public class Login
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
---
+++ RabbitMQ/Publishers/UserPublisher.cs
@@ -0,0 +1,41 @@
+﻿using AuthService.Models;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text.Json;
+
+namespace AuthService.RabbitMQ.Publishers
+{
+    public class UserPublisher
+    {
+        private const string QueueName = "user_created_queue";
+        public async Task PublishNewUserAsync(Guid userId, string name)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue: QueueName,
+                                             durable: true,
+                                             exclusive: false,
+                                             autoDelete: false,
+                                             arguments: null);
+
+            var userDto = new UserCreatedDto
+            {
+                UserId = userId,
+                UserName = name
+            };
+
+            string message = JsonSerializer.Serialize(userDto);
+            var body = System.Text.Encoding.UTF8.GetBytes(message);
+
+            await channel.BasicPublishAsync(exchange: string.Empty,
+                                             routingKey: QueueName,
+                                             mandatory: false,
+                                             basicProperties: new BasicProperties(),
+                                             body: new ReadOnlyMemory<byte>(body));
+
+            Console.WriteLine($" [x] Sent user ID: {userDto.UserId} - {userDto.UserName}");
+        }
+    }
+}
